da9093823762911721b08703fdb7124be76cd9c5
thetreedimensional <3dimensional@gmail.com>
2024-08-17 15:36:11 -0600

    atax_v2.py

--- atax/atax_v2.py
+++ atax/atax_v2.py
@@ -111,10 +111,10 @@ with depthai.Device(pipeline) as device:
                     # Stay asleep play snoring noise
                     prevpeople = npeople 
 
- """            for detection in detections:
+            for detection in detections:
                 bbox = frameNorm(frame, (detection.xmin, detection.ymin, detection.xmax, detection.ymax))
                 cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (255, 0, 0), 2)
-            cv2.imshow("preview", frame) """
+            cv2.imshow("preview", frame)
 
         if cv2.waitKey(1) == ord('q'):
             break
